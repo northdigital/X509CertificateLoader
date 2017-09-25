34f897f1cb7650b8044d96e6ea4ea10c6022b564
northdigital <giotismail@yahoo.com>
2017-09-25 23:08:49 +0300

    commit

--- src/gr/northdigital/essentials/Main.java
+++ src/gr/northdigital/essentials/Main.java
@@ -1,27 +1,41 @@
 package gr.northdigital.essentials;
 
-import com.sun.jndi.ldap.LdapName;
+import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.asn1.ASN1OctetString;
 
 import javax.naming.InvalidNameException;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
 public class Main {
+  public static String getExtensionValue(X509Certificate certificate, String oid) throws IOException {
+    byte[] bytes = certificate.getExtensionValue(oid);
+    if (bytes == null) {
+      return null;
+    }
+    ASN1InputStream aIn = new ASN1InputStream(new ByteArrayInputStream(bytes));
+    ASN1OctetString octs = (ASN1OctetString) aIn.readObject();
+    aIn = new ASN1InputStream(new ByteArrayInputStream(octs.getOctets()));
+    return aIn.readObject().toString();
+  }
 
-  public static void main(String[] args) throws CertificateException, FileNotFoundException, InvalidNameException {
+  public static void main(String[] args) throws CertificateException, IOException, InvalidNameException {
     CertificateFactory fact = CertificateFactory.getInstance("X.509");
     FileInputStream is = new FileInputStream("C:\\Users\\Panagiotis\\Documents\\ForBackup\\Projects\\northdigital\\ssl-demo\\demo.crt");
     X509Certificate cer = (X509Certificate) fact.generateCertificate(is);
 
-    String name = cer.getSubjectDN().getName();
+//    String name = cer.getSubjectDN().getName();
+//
+//    LdapName dn = new LdapName(name);
+//    System.out.println(dn + " has " + dn.size() + " RDNs: ");
+//    for (int i = 0; i < dn.size(); i++) {
+//      System.out.println(dn.get(i));
+//    }
 
-    LdapName dn = new LdapName(name);
-    System.out.println(dn + " has " + dn.size() + " RDNs: ");
-    for (int i = 0; i < dn.size(); i++) {
-      System.out.println(dn.get(i));
-    }
+    System.out.println(getExtensionValue(cer, "1.1.1.3.7"));
   }
 }
